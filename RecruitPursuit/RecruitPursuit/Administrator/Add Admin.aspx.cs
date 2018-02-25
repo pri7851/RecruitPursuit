aa8d6ebd2e0e51fd14e52019f55ed450513939a8
Myersth <mye5340@calu.edu>
2018-02-24 23:05:24 -0500

    small tweaks

--- RecruitPursuit/RecruitPursuit/Administrator/Add Admin.aspx.cs
+++ RecruitPursuit/RecruitPursuit/Administrator/Add Admin.aspx.cs
@@ -20,8 +20,7 @@ public partial class Administrator_Add_Admin : System.Web.UI.Page
     protected void btnAdd_Click(object sender, EventArgs e)
     {
         SqlDataSource1.InsertCommandType = SqlDataSourceCommandType.Text;
-        SqlDataSource1.InsertCommand = "INSERT into coach (Sport_id, C_FName, C_LName, C_title, Usename, Password) VALUES " +
-          "(@Sport_id, @C_FName, @C_LName, @C_title, @Usename, @Password)";
+        SqlDataSource1.InsertCommand = "INSERT into coach (Sport_id, C_FName, C_LName, C_title, Usename, Password) VALUES (@Sport_id, @C_FName, @C_LName, @C_title, @Usename, @Password)";
         SqlDataSource1.InsertParameters.Add("Sport_id", DropDownList1.SelectedValue);
         SqlDataSource1.InsertParameters.Add("C_FName", TextBox1.Text);
         SqlDataSource1.InsertParameters.Add("C_LName", TextBox2.Text);
