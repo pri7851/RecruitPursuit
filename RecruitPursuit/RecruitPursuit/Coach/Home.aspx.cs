6fde4d7976d748bf9561fc858812de1c95d56b82
wym9782 <WYM9782@calu.edu>
2018-04-05 14:14:58 -0400

    creating the rank page

--- RecruitPursuit/RecruitPursuit/Coach/Home.aspx.cs
+++ RecruitPursuit/RecruitPursuit/Coach/Home.aspx.cs
@@ -15,14 +15,7 @@ public partial class Home : System.Web.UI.Page
 
     protected void Page_Load(object sender, EventArgs e)
     {
-        if(Session["Username"] != null)
-        {
-            lblSession.Text = "Welcome " + Session["Username"].ToString() + "!";
-        }
-        else
-        {
-            Response.Redirect("User Login.aspx");
-        }
+       
     }
 
  
